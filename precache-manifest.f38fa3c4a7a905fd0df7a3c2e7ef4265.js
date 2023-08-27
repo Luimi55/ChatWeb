98c94637ccb7fb96e54f7ac7a01ef2226d763c05
Luimi55 <miguelrosa0105@gmail.com>
2023-08-27 10:52:01 -0400

    Updates

---
+++ precache-manifest.f38fa3c4a7a905fd0df7a3c2e7ef4265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d342891c212c48efc6c845010276e07",
+    "url": "/ChatWeb/index.html"
+  },
+  {
+    "revision": "1f2945be28f2f17d04ab",
+    "url": "/ChatWeb/static/css/main.5ae67725.chunk.css"
+  },
+  {
+    "revision": "bb88726ada76f1c62351",
+    "url": "/ChatWeb/static/js/2.34a6a6a5.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/ChatWeb/static/js/2.34a6a6a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2945be28f2f17d04ab",
+    "url": "/ChatWeb/static/js/main.a362275a.chunk.js"
+  },
+  {
+    "revision": "585f6068a013d4c944ad",
+    "url": "/ChatWeb/static/js/runtime-main.e2b492bd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46a4ad14112816f31e2403d5b825b912.js"
+  "/ChatWeb/precache-manifest.f38fa3c4a7a905fd0df7a3c2e7ef4265.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ChatWeb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
